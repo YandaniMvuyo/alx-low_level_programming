624cac0ddf674d7afae868122d96b82fd2c93ee4
YandaniMvuyo <Yandanimvuyo21@gmail.com>
2023-02-24 02:45:21 -0800

    isupplier

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,14 +1,16 @@
-0-isupper.c` | Checs for uppercase character |
-| `1-isdigit.c` | Checks for a digit (`0` through `9`) |
-| `2-mul.c` | Multiplies two integers |
-| `3-print_numbers.c` | Prints the numbers, from `0` to `9` |
-| `4-print_most_numbers.c` | Prints the numbers, from `0` to `9`, except `2` and `4` |
-| `5-more_numbers.c` | Prints 10 times the numbers, from `0` to `14` |
-| `6-print_line.c` | Draws a straight line in the terminal |
-| `7-print_diagonal.c` | Draws a diagonal line in the terminal |
-| `8-print_square.c` | Prints a sqaure |
-| `9-fizz_buzz.c` | Prints Fizz-Buzz test |
-| `10-print_triangle.c` | Prints a triangle |
-| `100-prime_factor.c` | Finds and prints the largest prime factor of the number `612852475143` |
-| `101-print_number.c` | Prints an integer |
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
